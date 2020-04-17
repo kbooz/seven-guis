889dec5b42901f82652c02d4145a5430423dc057
Lucas Inacio de Souza <lucas.inacio@accenture.com>
2020-04-16 22:13:07 -0300

    created base routing and started first challenge

---
+++ src/guis/Counter/Counter.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Counter from './Counter';
+
+describe('1 - Counter', () => {
+  it('should increment the number when clicked', () => {
+    const { findAllByTitle } = render(<Counter />);
+    // const button =
+  });
+});
---
+++ src/guis/Counter/Counter.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Counter() {
+  return <div>Olá</div>;
+}
+
+export default Counter;
